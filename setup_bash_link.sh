de3fd09ae823a1a8822ef730dd9200cf410dc241
MaximilienNaveau <maximilien.naveau@gmail.com>
2017-02-23 16:11:16 +0100

    add a bash file to save the current .bash* file and replace them by symbolic link toward the one in this repo

---
+++ setup_bash_link.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+pwd_local=$PWD
+tmpname="tmp_container"
+move () {
+    if [ -f $1 ] && [ ! -L $1 ]
+    then 
+	mv $1 $tmpname
+    else
+	echo "$1 is not a file or already a symbolik link"
+    fi
+} 
+
+# if ~/.bash_local is an existing directory
+if [ -d ~/.bash_local ]
+then
+    cd ~/.bash_local
+    if [ ! -d ~/.bash_local/tmp_container ]
+    then 
+	mkdir tmp_container
+    fi
+    move ~/.bash_aliases
+    move ~/.bash_ccache
+    move ~/.bash_logout
+    move ~/.bashrobotpkg
+    move ~/.bashrc tmp_container
+    cd ~
+    ln -s ~/.bash_local/bash_aliases .bash_aliases  
+    ln -s ~/.bash_local/bash_ccache .bash_ccache   
+    ln -s ~/.bash_local/bash_logout .bash_logout   
+    ln -s ~/.bash_local/bash_robotpkg .bash_robotpkg 
+    ln -s ~/.bash_local/bashrc .bashrc
+fi
+
+cd $pwd_local
