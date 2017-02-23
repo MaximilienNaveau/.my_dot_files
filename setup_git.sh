9896a135bbf412e6d1c83be13f8abd122246651f
MaximilienNaveau <maximilien.naveau@gmail.com>
2017-02-23 15:12:45 +0100

    add a script to initially setup git globally

---
+++ setup_git.sh
@@ -0,0 +1,4 @@
+git config --global user.email "maximilien.naveau@gmail.com"
+git config --global user.name "MaximilienNaveau"
+git config --global push.default matching
+git config --global core.editor emacs
